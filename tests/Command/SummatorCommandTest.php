c1506b175d04b5832024d1a10d581242171a23b4
Faner201 <62674880+Faner201@users.noreply.github.com>
2023-04-06 21:49:42 +1000

    Сreating tests covering business logic

---
+++ tests/Command/SummatorCommandTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Tests\Command;
+
+use App\Command\SummatorCommand;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class SummatorCommandTest extends TestCase
+{
+    public function testExecute(): void
+    {
+        $application = new Application();
+        $application->add(new SummatorCommand());
+
+        $command = $application->find('app:summator');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'directory' => 'tests/Fixtures/First',
+        ]);
+
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('Total counts: 15', $output);
+    }
+
+    public function testExecuteMultipleFiles(): void
+    {
+        $application = new Application();
+        $application->add(new SummatorCommand());
+
+        $command = $application->find('app:summator');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'directory' => 'tests/Fixtures/Second',
+        ]);
+
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('Total counts: 25', $output);
+    }
+
+    public function testExecuteInvalidPath(): void
+    {
+        $application = new Application();
+        $application->add(new SummatorCommand());
+
+        $command = $application->find('app:summator');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'directory' => 'tests/Three',
+        ]);
+        // $this->expectException('Invalid directory specified.');
+    }
+}
